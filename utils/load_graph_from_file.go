0fab2d6938469743e68a5d04514d9224de5ef730
troymcgahey <tmcgahey93@gmail.com>
2026-05-31 15:55:29 -0400

    Add load_graph_from_file.go Code

--- utils/load_graph_from_file.go
+++ utils/load_graph_from_file.go
@@ -1,1 +1,25 @@
 package utils
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func ReadNumbersFromFile(fileName string) ([]int, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var numbers []int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		num, err := strconv.Atoi(scanner.Text())
+		if err == nil {
+			numbers = append(numbers, num)
+		}
+	}
+	return numbers, scanner.Err()
+}
