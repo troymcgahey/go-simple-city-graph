8b8865445f83e83577d9d4ea2fdb3cdfb30e336b
troymcgahey <tmcgahey93@gmail.com>
2026-06-03 19:53:11 -0400

    Add BFS w/out lineage

--- internal/graph/bfs_graph.go
+++ internal/graph/bfs_graph.go
@@ -1,5 +1,25 @@
 package graph
 
-func SearchGraphBFS(cityList map[string]*CityNode, originCity string, destinationCity string) {
+func SearchGraphBFS(cityList map[string]*CityNode, startCity string, destinationCity string) bool {
 
+	visitedCity := make(map[string]bool)
+
+	searchCityList := []*CityNode{cityList[startCity]}
+
+	for len(searchCityList) > 0 {
+
+		currentCity := searchCityList[0]
+		searchCityList = searchCityList[1:]
+
+		if currentCity.City == destinationCity {
+			return true
+		}
+
+		visitedCity[currentCity.City] = true
+
+		searchCityList = append(searchCityList, currentCity.Connections...)
+
+	}
+
+	return false
 }
