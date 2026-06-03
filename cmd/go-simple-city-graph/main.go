ad41b857501e622fc3edb9f4971cd8fe850e64d1
troymcgahey <tmcgahey93@gmail.com>
2026-06-02 20:25:31 -0400

    Add command line arguments

--- cmd/go-simple-city-graph/main.go
+++ cmd/go-simple-city-graph/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"go-simple-city-graph/internal/graph"
 	"go-simple-city-graph/utils"
+	"os"
 )
 
 func main() {
@@ -16,6 +18,11 @@ func main() {
 	graph.CreateGraph(citiesArray, cityList)
 
 	//prompt for a starting city and find a path to a destination city
+
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: go run main.go Source_City Destination_City")
+		return
+	}
 
 	//bfs
 
