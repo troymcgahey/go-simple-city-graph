9a55d835155e224319c5e88f95587506d833d99a
troymcgahey <tmcgahey93@gmail.com>
2026-06-06 20:27:38 -0400

    Update with lineage

--- internal/graph/bfs_graph.go
+++ internal/graph/bfs_graph.go
@@ -1,9 +1,23 @@
 package graph
 
-func SearchGraphBFS(cityList map[string]*CityNode, startCity string, destinationCity string) bool {
+import "fmt"
 
-	//Initilize the searchCityList to be the startCity
-	searchCityList := []*CityNode{cityList[startCity]}
+func SearchGraphBFS(cityGraph map[string]*CityNode, startCity string, destinationCity string) bool {
+
+	type connectionCityNode struct {
+		cityNode      *CityNode
+		connectedFrom *CityNode
+	}
+
+	var newConnectionCity *connectionCityNode
+
+	newConnectionCity = &connectionCityNode{
+		cityNode: cityGraph[startCity],
+	}
+
+	searchCityList := []*connectionCityNode{newConnectionCity}
+
+	//for lineage, need to a reference for each CityNode, where it was connected from
 
 	for len(searchCityList) > 0 {
 
@@ -13,17 +27,28 @@ func SearchGraphBFS(cityList map[string]*CityNode, startCity string, destination
 		//Remove the currentCity from the searchCityList
 		searchCityList = searchCityList[1:]
 
-		if currentCity.City == "" {
+		if currentCity.cityNode.City == "" {
 			continue
 		}
 
-		if currentCity.City == destinationCity {
+		if currentCity.cityNode.City == destinationCity {
+
+			fmt.Println("Connected from ", currentCity.connectedFrom.City)
 			return true
 		}
 
-		//Add the currentCities Connections to the searchCityList
-		searchCityList = append(searchCityList, currentCity.Connections...)
+		for i, _ := range currentCity.cityNode.Connections {
+			//for len(currentCity.cityNode.Connections) > 0 {
 
+			newConnectionCity = &connectionCityNode{
+				cityNode:      currentCity.cityNode.Connections[i],
+				connectedFrom: currentCity.cityNode,
+			}
+
+			searchCityList = append(searchCityList, newConnectionCity)
+
+			i++
+		}
 	}
 
 	return false
